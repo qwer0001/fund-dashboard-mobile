ac5538789b1b1ba472c9b84f6f42c912ff71c14a
qwer0001 <42488611+qwer0001@users.noreply.github.com>
2026-08-20 15:24:36 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fund-dashboard-mobile-v2.2.0-cn-strategy';
+const CACHE='fund-dashboard-mobile-v2.3.0-cn-3day-history';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
